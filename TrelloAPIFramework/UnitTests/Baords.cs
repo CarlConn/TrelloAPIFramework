794c556322d16eb8ded3bacaf7b115e64a0f347d
Calr Conn <carlconn@aaawa.com>
2022-05-11 15:55:13 -0700

    added create a list

--- TrelloAPIFramework/UnitTests/Baords.cs
+++ TrelloAPIFramework/UnitTests/Baords.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
 
 namespace TrelloAPIFramework.UnitTests
@@ -8,19 +12,48 @@ namespace TrelloAPIFramework.UnitTests
     {
         public string url = "https://api.trello.com/";
         public string endPoint = "1/boards/";
-        private readonly string _KEY = "12766fee439ecb47c101c035a0b58af5";
+        private string boardName = "Practice";
+        private readonly string _key = "12766fee439ecb47c101c035a0b58af5";
         private readonly string _token = "790457a848b469ae775ebeb744a89f2b218e0ec73d9160021d9c62fef652aac4";
         
         [TestMethod]
-        public void CreateBoard()
+        public async Task CreateBoard()
         {
             var client = new RestClient(url);
-            var request = new RestRequest("/{{id}}/boards/", Method.Post);
-            request.AddParameter("id", 2);
-            request.AddQueryParameter("name", "CarlRider");
-            request.AddQueryParameter("key", _KEY);
+            var request = new RestRequest("/1/boards/", Method.Post);
+            request.AddQueryParameter("name", "CarlBoard");
+            request.AddQueryParameter("key", _key);
             request.AddQueryParameter("token", _token);
-            var response = await client.GetAsync(request);
+            var response = await client.PostAsync(request);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
         }
+   
+        [TestMethod]
+        public async Task BoardCreateList()
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest("1/list/", Method.Post);
+            request.AddQueryParameter("name", "Making a Copy");
+            request.AddQueryParameter("idBoard", "627c2f812ba2e94591831607");
+            request.AddQueryParameter("key", _key);
+            request.AddQueryParameter("token", _token);
+            var response = await client.PostAsync(request);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+        
+        [TestMethod]
+        public async Task DeleteBoard()
+        {
+           string uri = "/1/boards/" + "627c2eeea9488616286f4026" + "/";
+            
+           var client = new RestClient(url);
+           var request = new RestRequest(uri, Method.Delete);
+           request.AddQueryParameter("name", "CarlBoard");
+           request.AddQueryParameter("key", _key);
+           request.AddQueryParameter("token", _token);
+           var response = await client.DeleteAsync(request);
+           response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
     }
 }
